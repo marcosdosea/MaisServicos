281a6ba49e6dda6e54827fa608960fcd970bf482
Vitor Oliveira <vitorolive99@gmail.com>
2023-09-06 21:08:24 -0300

    Fazendo injecao de dependencias do bd, mapper e service da classe cliente em Program.cs. Fix #107

--- Codigo/MaisServicos/MaisServicosWeb/Program.cs
+++ Codigo/MaisServicos/MaisServicosWeb/Program.cs
@@ -1,27 +1,48 @@
-var builder = WebApplication.CreateBuilder(args);
+using Core;
+using Core.Service;
+using Microsoft.EntityFrameworkCore;
+using Service;
 
-// Add services to the container.
-builder.Services.AddControllersWithViews();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+namespace MaisServicosWeb
 {
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddControllersWithViews();
+
+            builder.Services.AddDbContext<MaisServicosContext>(
+                options => options.UseMySQL(builder.Configuration.GetConnectionString("MaisServicosDatabase")));
+
+            builder.Services.AddTransient<IClienteService, ClienteService>();
+
+            builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+
+            app.Run();
+        }
+    }
 }
-
-app.UseHttpsRedirection();
-app.UseStaticFiles();
-
-app.UseRouting();
-
-app.UseAuthorization();
-
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
-
-app.Run();
