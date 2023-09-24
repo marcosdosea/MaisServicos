4860967dfe9b4d452c85cafc0a57e2addf50fd65
TSjadness <jadsontavarests@hotmail.com>
2023-09-23 23:13:37 -0300

    implementaçao feita dos testes do Service e Controller do UC-Manter Prestador. Resolve #115.

---
+++ Codigo/MaisServicos/MaisServicosWebTests/Controllers/PrestadorControllerTests.cs
@@ -0,0 +1,301 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MaisServicosWeb.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using Core.Service;
+using Moq;
+using MaisServicosWeb.Mappers;
+using Core;
+using MaisServicosWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MaisServicosWeb.Controllers.Tests
+{
+    [TestClass()]
+    public class PrestadorControllerTests
+    {
+        private static PrestadorController controller;
+
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            //Arrange
+            var mockService = new Mock<IPrestadorService>();
+
+            IMapper mapper = new MapperConfiguration(cfg => cfg.AddProfile(new PrestadorProfile())).CreateMapper();
+
+            mockService.Setup(service => service.GetAll()).Returns(GetTestPrestadores());
+            mockService.Setup(service => service.Get(1)).Returns(GetTargetPrestador());
+            mockService.Setup(service => service.Edit(It.IsAny<Pessoa>())).Verifiable();
+            controller = new PrestadorController(mockService.Object, mapper);
+
+        }
+
+
+        [TestMethod()]
+        public void PrestadorControllerTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void IndexTest()
+        {
+            //Act
+            var result = controller.Index();
+
+            //Assert
+            Assert.IsInstanceOfType(result, typeof(ViewResult));
+            ViewResult viewResult = (ViewResult)result;
+            Assert.IsInstanceOfType(viewResult.ViewData.Model, typeof(List<PrestadorViewModel>));
+
+            List<PrestadorViewModel>? lista = (List<PrestadorViewModel>)viewResult.ViewData.Model;
+            Assert.AreEqual(3, lista.Count);
+        }
+
+        [TestMethod()]
+        public void DetailsTest()
+        {
+            //Act
+            var result = controller.Details(1);
+
+            //Assert
+            Assert.IsInstanceOfType(result, typeof(ViewResult));
+            ViewResult viewResult = (ViewResult)result;
+            Assert.IsInstanceOfType(viewResult.ViewData.Model, typeof(PrestadorViewModel));
+            PrestadorViewModel prestadorView = (PrestadorViewModel)viewResult.ViewData.Model;
+            Assert.AreEqual("Yuri Alberto", prestadorView.Nome);
+            Assert.AreEqual("bagrealberto@gmail.com", prestadorView.Email);
+            Assert.AreEqual("14735834556", prestadorView.Cpf);
+            Assert.AreEqual("13999873456", prestadorView.Telefone);
+            Assert.AreEqual("49000499", prestadorView.Cep);
+            Assert.AreEqual("SE", prestadorView.Estado);
+            Assert.AreEqual("Itabaiana", prestadorView.Cidade);
+            Assert.AreEqual("Marianga", prestadorView.Bairro);
+            Assert.AreEqual("Gaviões da fiel", prestadorView.Rua);
+            Assert.AreEqual("09", prestadorView.NumeroCasa);
+        }
+
+        [TestMethod()]
+        public void CreateTest()
+        {
+            // Act
+            var result = controller.Create();
+            // Assert
+            Assert.IsInstanceOfType(result, typeof(ViewResult));
+        }
+
+        [TestMethod()]
+        public void CreateTest_Valid()
+        {
+            //Act
+            var result = controller.Create(GetNewPrestador());
+
+            //Assert
+            Assert.IsInstanceOfType(result, typeof(RedirectToActionResult));
+            RedirectToActionResult redirectToActionResult = (RedirectToActionResult)result;
+            Assert.IsNull(redirectToActionResult.ControllerName);
+            Assert.AreEqual("Index", redirectToActionResult.ActionName);
+        }
+
+
+        [TestMethod()]
+        public void CreateTest_Invalid()
+        {
+            //Arrange
+            controller.ModelState.AddModelError("Nome", "Campo requerido");
+
+            //Act
+            var result = controller.Create(GetNewPrestador());
+
+            //Assert
+            Assert.AreEqual(1, controller.ModelState.ErrorCount);
+            Assert.IsInstanceOfType(result, typeof(RedirectToActionResult));
+            RedirectToActionResult redirectToActionResult = (RedirectToActionResult)result;
+            Assert.IsNull(redirectToActionResult.ControllerName);
+            Assert.AreEqual("Index", redirectToActionResult.ActionName);
+        }
+
+
+        [TestMethod()]
+        public void EditTest_Get()
+        {
+            //Act
+            var result = controller.Edit(1);
+
+            //Assert
+            Assert.IsInstanceOfType(result, typeof(ViewResult));
+            ViewResult viewResult = (ViewResult)result;
+            Assert.IsInstanceOfType(viewResult.ViewData.Model, typeof(PrestadorViewModel));
+            PrestadorViewModel prestadorView = (PrestadorViewModel)viewResult.ViewData.Model;
+            Assert.AreEqual("Yuri Alberto", prestadorView.Nome);
+            Assert.AreEqual("bagrealberto@gmail.com", prestadorView.Email);
+            Assert.AreEqual("14735834556", prestadorView.Cpf);
+            Assert.AreEqual("13999873456", prestadorView.Telefone);
+            Assert.AreEqual("49000499", prestadorView.Cep);
+            Assert.AreEqual("SE", prestadorView.Estado);
+            Assert.AreEqual("Itabaiana", prestadorView.Cidade);
+            Assert.AreEqual("Marianga", prestadorView.Bairro);
+            Assert.AreEqual("Gaviões da fiel", prestadorView.Rua);
+            Assert.AreEqual("09", prestadorView.NumeroCasa);
+        }
+
+        [TestMethod()]
+        public void EditTest_Post()
+        {
+            //Act
+            var result = controller.Edit(GetTargetPrestador().Id, GetTargetPrestadorViewModel());
+
+            //Assert
+            Assert.IsInstanceOfType(result, typeof(RedirectToActionResult));
+            RedirectToActionResult redirectToActionResult = (RedirectToActionResult)result;
+            Assert.IsNull(redirectToActionResult.ControllerName);
+            Assert.AreEqual("Index", redirectToActionResult.ActionName);
+        }
+
+        [TestMethod()]
+        public void DeleteTest_Post()
+        {
+            //Act
+            var result = controller.Delete(1);
+
+            //Assert
+            Assert.IsInstanceOfType(result, typeof(ViewResult));
+            ViewResult viewResult = (ViewResult)result;
+            Assert.IsInstanceOfType(viewResult.ViewData.Model, typeof(PrestadorViewModel));
+            PrestadorViewModel prestadorView = (PrestadorViewModel)viewResult.ViewData.Model;
+            Assert.AreEqual("Yuri Alberto", prestadorView.Nome);
+            Assert.AreEqual("bagrealberto@gmail.com", prestadorView.Email);
+            Assert.AreEqual("14735834556", prestadorView.Cpf);
+            Assert.AreEqual("13999873456", prestadorView.Telefone);
+            Assert.AreEqual("49000499", prestadorView.Cep);
+            Assert.AreEqual("SE", prestadorView.Estado);
+            Assert.AreEqual("Itabaiana", prestadorView.Cidade);
+            Assert.AreEqual("Marianga", prestadorView.Bairro);
+            Assert.AreEqual("Gaviões da fiel", prestadorView.Rua);
+            Assert.AreEqual("09", prestadorView.NumeroCasa);
+        }
+
+        [TestMethod()]
+        public void DeleteTest_Get()
+        {
+            // Act
+            var result = controller.Delete(GetTargetPrestadorViewModel().Id, GetTargetPrestadorViewModel());
+
+            // Assert
+            Assert.IsInstanceOfType(result, typeof(RedirectToActionResult));
+            RedirectToActionResult redirectToActionResult = (RedirectToActionResult)result;
+            Assert.IsNull(redirectToActionResult.ControllerName);
+            Assert.AreEqual("Index", redirectToActionResult.ActionName);
+        }
+
+        private PrestadorViewModel GetNewPrestador()
+        {
+            return new PrestadorViewModel
+            {
+                Id = 4,
+                Nome = "Fabio Santos",
+                Email = "tiochico@gmail.com",
+                Cpf = "32748726487",
+                Telefone = "87999871234",
+                Cep = "53699455",
+                Estado = "CE",
+                Cidade = "Foertaleza",
+                Bairro = "Invasão",
+                Rua = "Fiel Macabra",
+                NumeroCasa = "06"
+            };
+        }
+
+        private Pessoa GetTargetPrestador()
+        {
+            return new Pessoa
+            {
+                Id = 1,
+                Nome = "Yuri Alberto",
+                Email = "bagrealberto@gmail.com",
+                Cpf = "14735834556",
+                Telefone = "13999873456",
+                Cep = "49000499",
+                Estado = "SE",
+                Cidade = "Itabaiana",
+                Bairro = "Marianga",
+                Rua = "Gaviões da fiel",
+                Numero = "09"
+            };
+        }
+
+        private PrestadorViewModel GetTargetPrestadorViewModel()
+        {
+            return new PrestadorViewModel
+            {
+                Id = 2,
+                Nome = "Yuri Alberto",
+                Email = "bagrealberto@gmail.com",
+                Cpf = "14735834556",
+                Telefone = "13999873456",
+                Cep = "49000499",
+                Estado = "SE",
+                Cidade = "Itabaiana",
+                Bairro = "Marianga",
+                Rua = "Gaviões da fiel",
+                NumeroCasa = "09"
+            };
+        }
+
+        private IEnumerable<Pessoa> GetTestPrestadores()
+        {
+            return new List<Pessoa>
+            {
+                new Pessoa
+                {
+                    Id = 1,
+                    Nome = "Yuri Alberto",
+                    Email = "bagrealberto@gmail.com",
+                    Cpf = "14735834556",
+                    Telefone = "13999873456",
+                    Cep = "49000499",
+                    Estado = "SE",
+                    Cidade = "Itabaiana",
+                    Bairro = "Marianga",
+                    Rua = "Gaviões da fiel",
+                    Numero = "09"
+                },
+                new Pessoa
+                {
+                    Id = 2,
+                    Nome = "Renato Augusto",
+                    Email = "reinatoA@gmail.com",
+                    Cpf = "98587367423",
+                    Telefone = "79999765438",
+                    Cep = "49578300",
+                    Estado = "PE",
+                    Cidade = "São Domingos",
+                    Bairro = "Ponto Novo",
+                    Rua = "Camisa 12",
+                    Numero = "08"
+                },
+                new Pessoa
+                {
+                    Id = 1,
+                    Nome = "Cássio Ramos",
+                    Email = "gigantecassio@gmail.com",
+                    Cpf = "37498476136",
+                    Telefone = "75998439895",
+                    Cep = "49733744",
+                    Estado = "SP",
+                    Cidade = "São Paulo",
+                    Bairro = "Itaquera",
+                    Rua = "Pavilhão Nove",
+                    Numero = "12"
+                }
+            };
+        }
+
+    }
+}
---
+++ Codigo/MaisServicos/ServiceTests/PrestadorServiceTests.cs
@@ -0,0 +1,176 @@
+﻿using Core;
+using Core.Service;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service.Tests
+{
+    [TestClass()]
+    public class PrestadorServiceTests
+    {
+        private MaisServicosContext _context;
+        private IPrestadorService _prestadorService;
+
+        public void Initialize()
+        {
+            //Arrenge
+            var builder = new DbContextOptionsBuilder<MaisServicosContext>();
+            builder.UseInMemoryDatabase("MaisServicos");
+            var options = builder.Options;
+
+            _context = new MaisServicosContext(options);
+            _context.Database.EnsureDeleted();
+            _context.Database.EnsureCreated();
+            var prestadores = new List<Pessoa>
+            {
+                new Pessoa
+                {
+                    Id = 1,
+                    Nome = "Yuri Alberto",
+                    Email = "bagrealberto@gmail.com",
+                    Cpf = "14735834556",
+                    Telefone = "13999873456",
+                    Cep = "49000499",
+                    Estado = "SE",
+                    Cidade = "Itabaiana",
+                    Bairro = "Marianga",
+                    Rua = "Gaviões da fiel",
+                    Numero = "09"
+                },
+                new Pessoa
+                {
+                    Id = 2,
+                    Nome = "Renato Augusto",
+                    Email = "reinatoA@gmail.com",
+                    Cpf = "98587367423",
+                    Telefone = "79999765438",
+                    Cep = "49578300",
+                    Estado = "PE",
+                    Cidade = "São Domingos",
+                    Bairro = "Ponto Novo",
+                    Rua = "Camisa 12",
+                    Numero = "08"
+                },
+                new Pessoa
+                {
+                    Id = 3,
+                    Nome = "Cássio Ramos",
+                    Email = "gigantecassio@gmail.com",
+                    Cpf = "37498476136",
+                    Telefone = "75998439895",
+                    Cep = "49733744",
+                    Estado = "SP",
+                    Cidade = "São Paulo",
+                    Bairro = "Itaquera",
+                    Rua = "Pavilhão Nove",
+                    Numero = "12"
+                }
+            };
+            _context.AddRange(prestadores);
+            _context.SaveChanges();
+
+            _prestadorService = new PrestadorService(_context);
+        }
+
+
+        [TestMethod()]
+        public void CreateTest()
+        {
+            //Act
+            _prestadorService.Create(new Pessoa
+            {
+                Id = 4,
+                Nome = "Fabio Santos",
+                Email = "tiochico@gmail.com",
+                Cpf = "32748726487",
+                Telefone = "87999871234",
+                Cep = "53699455",
+                Estado = "CE",
+                Cidade = "Fortaleza",
+                Bairro = "Invasão",
+                Rua = "Fiel Macabra",
+                Numero = "06"
+            });
+
+            //Assert
+            Assert.AreEqual(4, _prestadorService.GetAll().Count());
+            var prestador = _prestadorService.Get(4);
+            Assert.AreEqual("Fabio Santos", prestador.Nome);
+            Assert.AreEqual("tiochico@gmail.com", prestador.Email);
+            Assert.AreEqual("32748726487", prestador.Cpf);
+            Assert.AreEqual("87999871234", prestador.Telefone);
+            Assert.AreEqual("53699455", prestador.Cep);
+            Assert.AreEqual("CE", prestador.Estado);
+            Assert.AreEqual("Fortaleza", prestador.Cidade);
+            Assert.AreEqual("Invasão", prestador.Bairro);
+            Assert.AreEqual("Fiel Macabra", prestador.Rua);
+            Assert.AreEqual("06", prestador.Numero);
+        }
+
+        [TestMethod()]
+        public void DeleteTest()
+        {
+            //Act
+            _prestadorService.Delete(2);
+            //Assert
+            Assert.AreEqual(2, _prestadorService.GetAll().Count());
+            var prestador = _prestadorService.Get(2);
+            Assert.AreEqual(null, prestador);
+        }
+
+        [TestMethod()]
+        public void EditTest()
+        {
+            //Act
+            var prestador = _prestadorService.Get(3);
+            prestador.Nome = "Carlos Miguel";
+            prestador.Email = "eternoreserva@gmail.com";
+            prestador.Numero = "22";
+            _prestadorService.Edit(prestador);
+
+            //Assert
+            prestador = _prestadorService.Get(3);
+            Assert.IsNotNull(prestador);
+            Assert.AreEqual("Carlos Miguel", prestador.Nome);
+            Assert.AreEqual("eternoreserva@gmail.com", prestador.Email);
+            Assert.AreEqual("22", prestador.Numero);
+        }
+
+        [TestMethod()]
+        public void GetTest()
+        {
+            var prestador = _prestadorService.Get(1);
+            Assert.IsNotNull(prestador);
+            Assert.AreEqual("Yuri Alberto", prestador.Nome);
+            Assert.AreEqual("bagrealberto@gmail.com", prestador.Email);
+            Assert.AreEqual("14735834556", prestador.Cpf);
+            Assert.AreEqual("13999873456", prestador.Telefone);
+            Assert.AreEqual("49000499", prestador.Cep);
+            Assert.AreEqual("SE", prestador.Estado);
+            Assert.AreEqual("Itabaiana", prestador.Cidade);
+            Assert.AreEqual("Marianga", prestador.Bairro);
+            Assert.AreEqual("Gaviões da fiel", prestador.Rua);
+            Assert.AreEqual("09", prestador.Numero);
+        }
+
+        [TestMethod()]
+        public void GetAllTest()
+        {
+            //Act
+            var listaPrestador = _prestadorService.GetAll();
+            //Assert
+            Assert.IsInstanceOfType(listaPrestador, typeof(IEnumerable<Pessoa>));
+            Assert.IsNotNull(listaPrestador);
+            Assert.AreEqual(3, listaPrestador.Count());
+            Assert.AreEqual(1, listaPrestador.First().Id);
+            Assert.AreEqual("Yuri Alberto", listaPrestador.First().Nome);
+        }
+
+    }
+}
