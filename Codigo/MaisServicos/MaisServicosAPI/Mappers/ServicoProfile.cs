be57c03caef48eeb955a8aaa537d59100ab09c50
CaioMorais02 <caioedpm@gmail.com>
2023-10-15 21:15:26 -0300

    Controller, mapper e model da API de Servico implementado

---
+++ Codigo/MaisServicos/MaisServicosAPI/Mappers/ServicoProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using Core;
+using MaisServicosWeb.Models;
+
+namespace MaisServicosWeb.Mappers
+{
+    public class ServicoProfile : Profile
+    {
+        public ServicoProfile()
+        {
+            CreateMap<ServicoViewModel, Pessoa>().ReverseMap();
+        }
+    }
+}
+
