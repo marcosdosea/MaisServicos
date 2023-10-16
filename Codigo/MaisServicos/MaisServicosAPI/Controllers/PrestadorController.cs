251167b226f3ea6d7c2631b2d509a8dd0b2f8073
TSjadness <jadsontavarests@hotmail.com>
2023-10-16 14:42:53 -0300

    Implementaçao feita da API do UC-Manter Prestador. Resolve #143.

---
+++ Codigo/MaisServicos/MaisServicosAPI/Controllers/PrestadorController.cs
@@ -0,0 +1,101 @@
+﻿using AutoMapper;
+using Core;
+using Core.Service;
+using MaisServicosAPI.Models;
+using MaisServicosWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MaisServicosAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PrestadorController : ControllerBase
+    {
+
+        private readonly IPrestadorService _prestadorService;
+        private readonly IMapper _mapper;
+
+        public PrestadorController(IPrestadorService prestadorService, IMapper mapper)
+        {
+            _prestadorService = prestadorService;
+            _mapper = mapper;
+        }
+
+
+
+        // GET: api/<PrestadorController>
+        [HttpGet]
+        public ActionResult Get()
+        {
+            var listPrestador = _prestadorService.GetAll();
+            if (listPrestador == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(listPrestador);
+        }
+
+        // GET api/<PrestadorController>/5
+        [HttpGet("{id}")]
+        public ActionResult Get(int id)
+        {
+            Pessoa prestador = _prestadorService.Get(id);
+            if(prestador == null)
+            {
+                return NotFound();
+            }
+            return Ok(prestador);
+        }
+
+        // POST api/<PrestadorController>
+        [HttpPost]
+        public ActionResult Post([FromBody] PrestadorViewModel prestadorViewModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest("Dados invalidos");
+            }
+
+            var prestador = _mapper.Map<Pessoa>(prestadorViewModel);
+            _prestadorService.Create(prestador);
+
+            return Ok(prestador);
+        }
+
+        // PUT api/<PrestadorController>/5
+        [HttpPut("{id}")]
+        public ActionResult Put(int id, [FromBody] PrestadorViewModel prestadorViewModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest("Dados invalidos");
+            }
+
+            var prestador = _mapper.Map<Pessoa>(prestadorViewModel);
+            if (prestador == null)
+            {
+                return NotFound();
+            }
+            _prestadorService.Create(prestador);
+
+            return Ok(prestador);
+        }
+
+        // DELETE api/<PrestadorController>/5
+        [HttpDelete("{id}")]
+        public ActionResult Delete(int id)
+        {
+            Pessoa prestador = _prestadorService.Get(id);
+            if (prestador == null)
+            {
+                NotFound();
+            }
+
+            _prestadorService.Delete(id);
+            return Ok();
+        }
+    }
+}
---
+++ Codigo/MaisServicos/MaisServicosAPI/Models/PrestadorViewModel.cs
@@ -0,0 +1,54 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MaisServicosAPI.Models
+{
+    public class PrestadorViewModel
+    {
+        [Display(Name = "Código")]
+        [Key]
+        [Required(ErrorMessage = "Este campo é obrigatório.")]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Este campo é obrigatório")]
+        [StringLength(50, MinimumLength = 2, ErrorMessage = "O nome do cliente deve ter entre 2 e 50 caracteres.")]
+        public string Nome { get; set; } = null!;
+
+        [StringLength(50)]
+        [Required(ErrorMessage = "Este campo é obrigatório.")]
+        public string Email { get; set; } = null!;
+
+        [Display(Name = "CPF")]
+        [StringLength(11, MinimumLength = 11, ErrorMessage = "Campo CPF inválido.")]
+        [Required(ErrorMessage = "Este campo é obrigatório.")]
+        public string Cpf { get; set; } = null!;
+
+        [StringLength(11, MinimumLength = 11, ErrorMessage = "Campo telefone inválido.")]
+        public string? Telefone { get; set; }
+
+        [Display(Name = "CEP")]
+        [StringLength(8, MinimumLength = 8, ErrorMessage = "Campo CEP inválido.")]
+        [Required(ErrorMessage = "Este campo é obrigatório.")]
+        public string Cep { get; set; } = null!;
+
+        [StringLength(2, MinimumLength = 2, ErrorMessage = "Campo estado inválido.")]
+        [Required(ErrorMessage = "Este campo é obrigatório.")]
+        public string Estado { get; set; } = null!;
+
+        [StringLength(28, MinimumLength = 1, ErrorMessage = "Campo cidade inválido.")]
+        [Required(ErrorMessage = "Este campo é obrigatório.")]
+        public string Cidade { get; set; } = null!;
+
+        [StringLength(50, MinimumLength = 1, ErrorMessage = "Campo bairro inválido.")]
+        [Required(ErrorMessage = "Este campo é obrigatório.")]
+        public string Bairro { get; set; } = null!;
+
+        [StringLength(50, MinimumLength = 1, ErrorMessage = "Campo rua inválido.")]
+        [Required(ErrorMessage = "Este campo é obrigatório.")]
+        public string Rua { get; set; } = null!;
+
+        [Display(Name = "Número")]
+        [StringLength(50, MinimumLength = 1, ErrorMessage = "Campo número inválido.")]
+        [Required(ErrorMessage = "Este campo é obrigatório.")]
+        public string Numero { get; set; } = null!;
+    }
+}
